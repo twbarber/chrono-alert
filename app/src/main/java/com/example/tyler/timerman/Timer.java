950b39195e64afca1fe5a8c66d6feead1522bf85
Tyler Barber <dev@twbarber.com>
2016-11-26 18:18:42 -0600

    Added start/stop buttons and customized the time field.

--- app/src/main/java/com/example/tyler/timerman/Timer.java
+++ app/src/main/java/com/example/tyler/timerman/Timer.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Timer extends AppCompatActivity {
@@ -13,38 +15,59 @@ public class Timer extends AppCompatActivity {
     private static final String TAG = "Timer";
 
     private Chronometer chronometer;
+    private TextView durationEntry;
+    private Button startButton;
+    private Button stopButton;
+
     private boolean started = false;
-    private long startTime;
+    private long stopTime;
+    private long duration;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        this.chronometer = (Chronometer) this.findViewById(R.id.chrono);
-        this.chronometer.setOnClickListener(new View.OnClickListener() {
+
+        this.durationEntry = (TextView) this.findViewById(R.id.second_counter);
+
+        this.startButton = (Button) findViewById(R.id.start);
+        this.startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (started) {
-                    chronometer.stop();
-                    startTime  = SystemClock.elapsedRealtime();
-                    started = false;
-                } else {
-                    if(startTime == 0) {
-                        startTime = SystemClock.elapsedRealtime();
+            public void onClick(View v) {
+                duration = Long.parseLong(durationEntry.getText().toString());
+                if(!started) {
+                    if(stopTime == 0) {
+                        chronometer.setBase(SystemClock.elapsedRealtime());
+                    } else {
+                        chronometer.setBase(
+                                SystemClock.elapsedRealtime() - stopTime + chronometer.getBase());
                     }
-                    chronometer.setBase(startTime);
                     chronometer.start();
                     started = true;
                 }
             }
         });
 
+        this.stopButton = (Button) findViewById(R.id.stop);
+        this.stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (started) {
+                    stopTime = SystemClock.elapsedRealtime();
+                    chronometer.stop();
+                    started = false;
+                }
+            }
+        });
+
+        this.chronometer = (Chronometer) this.findViewById(R.id.chrono);
+
         this.chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 long seconds = (SystemClock.elapsedRealtime() - chronometer.getBase()) / 1000;
                 Log.i(TAG, seconds + " seconds have elapsed.");
-                if (seconds != 0 && seconds % 5 == 0) {
+                if (seconds != 0 && seconds % duration == 0) {
                     Toast.makeText(getApplicationContext(),
                             String.format("%s Seconds Have Elapsed", seconds),
                             Toast.LENGTH_SHORT).show();
